1a03ea36ceab402f190b9fe1e1224b73f0768df9
robin-msk <robin.musyoki@kibo.school>
2023-02-22 14:14:50 +0300

    removed lint from vscode extenssions

--- webapp.py
+++ webapp.py
@@ -1,14 +1,16 @@
-from flask import Flask, render_template, request 
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-# a get request with a variable id 
+# a starightforward get request
 
-@app.get('/pets/<animal_id>')
-def pet(animal_id):
-    return "This is a pet page"
-
-# a starightforward get request 
 
 @app.get('/')
 def user():
     return "This is a profile page"
+
+# a get request with a variable id
+
+
+@app.get('/pets/<animal_id>')
+def pet(animal_id):
+    return "This is a pet page"
